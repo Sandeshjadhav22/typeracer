f3b91573cd0c6aa230d4017b9bc02175f871db4f
sandy <sandeshjadhav2002@gmail.com>
2024-03-21 20:45:32 +0530

    player typed functions added

--- apps/server/classes/game.ts
+++ apps/server/classes/game.ts
@@ -1,4 +1,5 @@
 import { Server, Socket } from "socket.io";
+import { generateParagraph } from "../utils/genrateParagraph";
 
 export class Game {
   gameStatus: "not-started" | "in-progress" | "finished";
@@ -17,7 +18,56 @@ export class Game {
     this.paragraph = "";
   }
 
-  setupListeners(socket: Socket) {}
+  setupListeners(socket: Socket) {
+    socket.on("start-game", async () => {
+      if (this.gameStatus === "in-progress")
+        return socket.emit("error", "The game has already started");
+
+      if (this.gameHost !== socket.id)
+        return socket.emit(
+          "error",
+          "You are not the host of the game. Only the host can start the game"
+        );
+
+      for (const player of this.players) {
+        player.score = 0;
+      }
+
+      this.io.to(this.gameId).emit("players", this.players);
+
+      this.gameStatus = "in-progress";
+
+      const paragraph = await generateParagraph();
+
+      this.paragraph = paragraph;
+      this.io.to(this.gameId).emit("game-started", paragraph);
+
+      setTimeout(() => {
+        this.gameStatus = "finished";
+        this.io.to(this.gameId).emit("game-finished");
+        this.io.to(this.gameId).emit("players", this.players);
+      }, 60000);
+    });
+
+    socket.on("player-typed", (typed: string) => {
+      if (this.gameStatus !== "in-progress")
+        return socket.emit("error", "this game has not started yet");
+
+        const splittedParagraph = this.paragraph.split(" ");
+        const splittedTyped = typed.split(" ");
+  
+        let score = 0;
+  
+        for (let i = 0; i < splittedTyped.length; i++) {
+          if (splittedTyped[i] === splittedParagraph[i]) {
+            score++;
+          } else {
+            break;
+          }
+        }
+    });
+  }
+
   joinPlayer(id: string, name: string, socket: Socket) {
     if (this.gameStatus === "in-progress")
       return socket.emit(
@@ -25,15 +75,17 @@ export class Game {
         "Game has already started please wait for it to end before joining"
       );
 
-      this.players.push({id,name,score:0})
+    this.players.push({ id, name, score: 0 });
 
-      this.io.to(this.gameId).emit('player-joined',{
-        id, name, score: 0
-      });
+    this.io.to(this.gameId).emit("player-joined", {
+      id,
+      name,
+      score: 0,
+    });
 
-      socket.emit('player',this.players)
-      socket.emit('new-host',this.gameHost)
+    socket.emit("player", this.players);
+    socket.emit("new-host", this.gameHost);
 
-      this.setupListeners(socket)
+    this.setupListeners(socket);
   }
 }
